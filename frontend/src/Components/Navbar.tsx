403898147b50965e4f4229e95ca8f40a4105bc7e
Alex <98610827+MindSetter77@users.noreply.github.com>
2025-05-14 16:48:39 +0200

    Final 2.0

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { motion, styleEffect } from 'framer-motion';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 
 interface NavbarProps {
@@ -13,23 +13,32 @@ interface NavbarProps {
 
 function Navbar({setGeneratedEmail, setError, language, setLanguage} : NavbarProps) {
     const navigate = useNavigate();
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    
+    // cleanup przy odmontowaniu
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   
   return (  
     <motion.header 
       initial={{ opacity: 0, y: 0 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 5, type: 'spring' }}
-      style={{ display: 'flex',  position: 'absolute', justifyContent: 'space-between', alignItems: 'center', padding: '1rem 2rem', zIndex: 5, borderBottom: '1px solid rgba(255,255,255,0.1)', width: '100vw' }}>
-        <h2 style={{ fontSize: '1.5rem', margin: 0, cursor: 'pointer', color: 'white' }} onClick={() => {setGeneratedEmail(''); setError(false); navigate('/')}}>📨 MailPilot AI</h2>
-        <nav style={{ display: 'flex'}}>
-          <div style={{display: 'flex', alignItems: 'center', gap: '1rem'}}>
+      style={{ display: 'flex',  position: 'absolute', justifyContent: 'space-between', alignItems: 'center', zIndex: 5, borderBottom: '1px solid rgba(255,255,255,0.1)', width: '100vw', height: '60px ' }}>
+        <h2 style={{ fontSize: '1.5rem', margin: 0, cursor: 'pointer', color: 'white', marginLeft: window.innerWidth < 540 ? '10px' : '70px' }} onClick={() => {setGeneratedEmail(''); setError(false); navigate('/')}}>📨 MailPilot</h2>
+        <div style={{ display: 'flex'}}>
+          <div style={{display: 'flex', alignItems: 'center', gap: '1rem', width: '100%'}}>
           <div onClick={() => {language === 'EN' ? setLanguage('PL') : setLanguage('EN')}} style={{cursor: 'pointer'}}>
             <img src={language === 'EN' ? ('uk-flag.png') : ('pl-flag.png')} style={{width: '40px'}} />
           </div>
-          <a onClick={() => navigate('/')} style={{ color: 'white', textDecoration: 'none', opacity: 0.8, cursor: 'pointer', fontSize: '21px' }}>{language === 'EN' ? ('Home') : ('Strona główna')}</a>
-          <div onClick={() => navigate('/about')}><a   style={{ color: 'white', textDecoration: 'none', opacity: 0.8, marginRight: '100px', cursor: 'pointer', fontSize: '21px' }}>{language === 'EN' ? ('About') : ('O stronie')}</a></div>
+          <a onClick={() => navigate('/')} style={{ color: 'white', textDecoration: 'none', opacity: 0.8, cursor: 'pointer', fontSize: '21px' }}>Home</a>
+          <div onClick={() => navigate('/about')}><a   style={{ color: 'white', textDecoration: 'none', opacity: 0.8, marginRight: window.innerWidth < 540 ? '10px' : '70px', cursor: 'pointer', fontSize: '21px' }}>{language === 'EN' ? ('About') : ('O nas')}</a></div>
           </div>
-        </nav>
+        </div>
       </motion.header>
   );
 }
